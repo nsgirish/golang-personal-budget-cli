35668feaf8232f245ffd3a83ff80c6cc89ad4339
girish ns <nsgirish@gmail.com>
2021-12-12 22:40:39 +0800

    some changes

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,14 @@
 package module1
 
 // Budget stores budget information
+type struct Budget {
+	Max float32
+	Items []Item
+}
 
 // Item stores item information
+type struct Item {
+	Description string
+	Price float32 
+
+}
