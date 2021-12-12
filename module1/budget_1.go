a115c5c7e107ad63609796fed2c430e423ec95fc
girish ns <nsgirish@gmail.com>
2021-12-12 22:48:50 +0800

    syntax corrections

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,14 +1,13 @@
 package module1
 
 // Budget stores budget information
-type struct Budget {
-	Max float32
+type Budget struct {
+	Max   float32
 	Items []Item
 }
 
 // Item stores item information
-type struct Item {
+type Item struct {
 	Description string
-	Price float32 
-
+	Price       float32
 }
